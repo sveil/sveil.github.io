4f35f2c923c3dd8338eb86ec01867f4ddab16902
boy12371 <boy12371@hotmail.com>
2020-11-24 01:30:04 +0800

    first commit

---
+++ component---src-templates-page-404-tsx-640d0b8a11fcfa56a920.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"5z2n":function(n,e,t){"use strict";t.r(e);var u=t("q1tI"),a=t.n(u);e.default=function(){return a.a.createElement("div",null,"404")}}}]);
